3bccfb2aa5a962d408410a56015c0bbf524ad2dc
KianVille <56126317+KianVille@users.noreply.github.com>
2020-06-01 19:38:30 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
-$("#btn_bold").click(function() {
-  $("button[aria-labelledby='ck-editor__aria-label_ed3cda37341c5b7cb6141a20889ecad87']").click();
-});
+let output;
+
+$(".ck-dropdown__panel").css("left","-130px");
+$("#getCodeBtn").click(function(){
+  output = new Convert();
+  output.remove("p");
+  output.convert("strong");
+  output.convert("i");
+  output.convert("u");
+  output.convert("s");
+  output.convertColor();
+  $("#codeHere").html(output.getCode())
+
+})
+
+$("#copyBtn").click(function(){
+if ($("#codeHere").val()) {
+  /* Get the text field */
+ var copyText = document.getElementById("codeHere");
+
+ /* Select the text field */
+ copyText.select();
+ copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+ /* Copy the text inside the text field */
+ document.execCommand("copy");
+
+ /* Alert the copied text */
+ alert("Copied! Now paste it on ML!");
+}else {
+    output = new Convert();
+  if (output.data) {
+    alert("Click Get Code!")
+  }else {
+    alert("Type and Edit something!")
+  }
+}
+})
